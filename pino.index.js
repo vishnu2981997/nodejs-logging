a6935bbd6d481ab613d49dd364679f077009c185
vishnu2981997 <vishnu2981997@gmail.com>
2023-01-04 19:01:32 -0800

    * Updated pino index logs

--- pino.index.js
+++ pino.index.js
@@ -11,17 +11,17 @@ app.use(contextMiddleware);
 app.use(requestLoggerMiddleware({logger}))
 
 app.get('/pino', async function (req, res) {
-    logger.info(req, 'log 1');
-    logger.info(req, 'log 2');
-    logger.info(req, 'log 3');
-    logger.info(req, 'log 4');
-    logger.info(req, 'log 5');
+    logger.info('log 1');
+    logger.info('log 2');
+    logger.info('log 3');
+    logger.info('log 4');
+    logger.info('log 5');
     await new Promise(resolve => setTimeout(resolve, 500));
-    logger.info(req, 'log 6');
-    logger.info(req, 'log 7');
-    logger.info(req, 'log 8');
-    logger.info(req, 'log 9');
-    logger.info(req, 'log 10');
+    logger.info('log 6');
+    logger.info('log 7');
+    logger.info('log 8');
+    logger.info('log 9');
+    logger.info('log 10');
     res.status(200).send({});
 });
 
